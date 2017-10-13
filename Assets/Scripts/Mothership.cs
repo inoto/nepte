6d0160e084e0e7ca09f47ef5a095c3708b9e38d4
dimka <tabakov.dmitriy@sonakai.com>
2017-10-13 19:55:07 +0300

    - started mothership

---
+++ Assets/Scripts/Mothership.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+
+public class Mothership : MonoBehaviour, ITargetable
+{
+	public static ConfigDrone config;
+	
+    public enum Mode
+    {
+        Idle,
+        Moving,
+        Attacking,
+        Dead
+    }
+	public Mode mode;
+
+	private bool isStarted = false;
+	public GameObject explosionPrefab;
+
+    [Header("Modules")]
+    public Health health;
+	public CollisionCircle collision;
+
+    [Header("Components")]
+    public Transform trans;
+    MeshRenderer mesh;
+    public Owner owner;
+	public Weapon weapon;
+
+	[Header("Cache")]
+	public Base bas;
+	public Base basTarget;
+	public GameObject assignedCircle;
+	public float angle = 0;
+	public float speed = 0.3f;
+	public float radius = 2.5f;
+
+	[Header("Colors")]
+	[SerializeField]
+	private Material[] materials;
+
+    private void Awake()
+    {
+		trans = GetComponent<Transform>();
+		mesh = GetComponent<MeshRenderer>();
+        owner = GetComponent<Owner>();
+//        body = GetComponent<Body>();
+//        radar = GetComponent<Radar>();
+	    weapon = GetComponent<Weapon>();
+	    assignedCircle = GameObject.Find("MothershipCircle");
+	    //config = ConfigManager.Instance.Drone;
+    }
+
+//	private void Start()
+//	{
+//		Debug.Log("start");
+//	}
+
+	public void DelayedStart()
+    {
+        //SetOwnerAsInParent();
+        AssignMaterial();
+        owner.playerController.playerUnitCount += 1;
+        PlayerController.unitCount += 1;
+    }
+
+	private void OnEnable()
+	{
+		mode = Mode.Idle;
+		collision = new CollisionCircle(trans, null, owner, null);
+		CollisionManager.Instance.AddCollidable(collision);
+		collision.isDead = false;
+		collision.collidedBaseCircle = null;
+		
+//		LoadFromConfig();
+//		Debug.Log("on enable");
+	}
+
+	private void Update()
+	{
+		angle += Time.deltaTime;
+
+		var x = Mathf.Cos (angle * speed) * radius;
+		var y = Mathf.Sin (angle * speed) * radius;
+		transform.position = new Vector2(x, y);
+	}
+
+	void LoadFromConfig()
+	{
+		if (config == null)
+			return;
+		if (health != null)
+		{
+			health.max = config.HealthMax;
+			health.current = health.max;
+		}
+		if (weapon != null)
+		{
+			weapon.attackSpeed = config.AttackSpeed;
+			weapon.damage = config.AttackDamage;
+			weapon.damageNoBonuses = weapon.damage;
+			weapon.radius = config.AttackRadius;
+			weapon.missilePrefabName = config.AttackMissilePrefabName;
+			weapon.missilePrefab = Resources.Load<GameObject>(weapon.missilePrefabName);
+		}
+	}
+
+	public void Die()
+	{
+		mode = Mode.Dead;
+		collision.isDead = true;
+		weapon.collision.isDead = true;
+//		if (collision.collidedBaseCircle != null)
+//		{
+//			Capture capture = collision.collidedBaseCircle.trans.GetComponent<Capture>();
+//			capture.RemoveCapturerByPlayer(owner.playerNumber);
+//			capture.bas.collision.collidedCount--;
+//			collision.collidedBaseCircle = null;
+//		}
+		if (health.current <= 0)
+			MakeExplosion();
+		owner.playerController.playerUnitCount -= 1;
+		PlayerController.unitCount -= 1;
+		ObjectPool.Recycle(gameObject);
+	}
+
+	void MakeExplosion()
+	{
+		GameObject explosion = Instantiate(explosionPrefab, trans.position, trans.rotation);
+		explosion.transform.parent = GameController.Instance.transform;
+		float size = GetComponent<QuadMesh>().size * 1.5f;
+		explosion.transform.localScale = new Vector3(size, size, 1);
+	}
+
+	void SetOwnerAsInParent()
+	{
+		var ownerParent = trans.parent.GetComponent<Owner>();
+		owner.playerNumber = ownerParent.playerNumber;
+		owner.playerController = ownerParent.playerController;
+	}
+
+	void AssignMaterial()
+	{
+		if (mesh != null && owner != null)
+			mesh.sharedMaterial = materials[owner.playerNumber];
+		else
+			Debug.LogError("Cannot assign material.");
+	}
+
+	public void Damage(Weapon _weapon)
+	{
+		health.current -= _weapon.damage;
+		if (health.current <= 0)
+		{
+			Die();
+			_weapon.EndCombat();
+		}
+	}
+	
+	public void Damage(int damage)
+	{
+		health.current -= damage;
+		if (health.current <= 0)
+		{
+			Die();
+		}
+	}
+
+	public GameObject GameObj
+	{
+		get { return gameObject; }
+	}
+
+	public bool IsDied
+	{
+		get { return (mode == Mode.Dead); }
+	}
+
+	private void OnDrawGizmos()
+	{
+		if (collision != null)
+			collision.DrawGizmos();
+	}
+}
